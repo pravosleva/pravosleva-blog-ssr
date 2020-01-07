a79fd248160465409a519607fe1412a3c4490063
Den Pol <selection4test@gmail.com>
2020-01-07 21:32:05 +0300

    master. fix

--- envs-init.sh
+++ envs-init.sh
@@ -85,14 +85,13 @@ then
             DATABASE_SSL: false,
           }
         }, {
-          name: 'blog-3000-dev',
+          name: 'blog-9000-dev',
           cwd: __dirname + '/frontend',
           script: 'yarn',
           args: 'start',
           interpreter: 'none',
           env: {
             NODE_ENV: 'development',
-            PORT: '3000',
             REACT_APP_API_ENDPOINT: 'http://localhost:1337',
           }
         }],
@@ -116,14 +115,13 @@ then
             DATABASE_SSL: false,
           }
         }, {
-          name: 'blog-3000-prod',
+          name: 'blog-9000-prod',
           cwd: __dirname + '/frontend',
           script: 'yarn',
           args: 'serve',
           interpreter: 'none',
           env: {
             NODE_ENV: 'production',
-            PORT: '3000',
             REACT_APP_API_ENDPOINT: 'http://80.87.194.181/api',
           }
         }],
